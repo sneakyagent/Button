5b0609ea6b0e218640f191219f1c06fa250991eb
sneakyagent <31023605+sneakyagent@users.noreply.github.com>
2019-04-13 09:31:30 -0600

    Add files via upload

---
+++ BUTTON.ino
@@ -0,0 +1,22 @@
+int buttonPin = 2;     // the number of the pushbutton pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+   Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    Serial.println("On");
+  } else {
+   Serial.println("Off");
+  }
+}
